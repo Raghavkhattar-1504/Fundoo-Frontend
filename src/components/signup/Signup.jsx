4b89bf5317c2b5560c6646f8d2d6542ce828b857
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-03-08 09:38:17 +0530

    [Raghav] : REFACTOR - Fixed some issues

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './signup.scss'
+import './Signup.scss'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Googleone from '../../assets/googleone.png'
